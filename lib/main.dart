e01240bafde6e4321aec1b12a5cd440abb073c5a
wendi210 <nengahwendiana22@gmail.com>
2020-05-10 12:45:37 +0800

    crud_api

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:crud_api/src/app.dart';
+
+void main() => runApp(App());
---
+++ lib/src/app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:crud_api/src/ui/formadd/form_add_screen.dart';
+import 'package:crud_api/src/ui/home/home_screen.dart';
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        accentColor: Colors.blueAccent,
+      ),
+      home: Scaffold(
+        key: _scaffoldState,
+        appBar: AppBar(
+          title: Text(
+            "MAHASISWA PTI",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  _scaffoldState.currentContext,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return FormAddScreen();
+                  }),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
